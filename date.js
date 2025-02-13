3366ac6455c457e9aeb4d4dfd8dfa621f2e6a548
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-02-14 02:32:16 +0600

    swap and date added

---
+++ date.js
@@ -0,0 +1,8 @@
+const today = new Date();
+const date = new Date("2062-10-09");
+console.log(today.getMonth());
+
+const specificDate = new Date("2091-10-26");
+console.log(specificDate);
+
+// unix epoc
