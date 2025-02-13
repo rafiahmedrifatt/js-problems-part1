e7640766e445738436d8e52cee9b54773bba1999
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-02-14 02:13:51 +0600

    math added

---
+++ math.js
@@ -0,0 +1,11 @@
+const min = Math.min(45, 6, 76, 78, 4, 53, 45, 675, 778, 34, 3, 2, 1);
+const max = Math.max(45, 6, 76, 78, 4, 53, 45, 675, 778, 34, 3, 2, 1);
+
+console.log(min);
+console.log(max);
+console.log(Math.PI);
+console.log(Math.abs(-5 - 5));
+console.log(Math.round(45.8)); //46
+console.log(Math.floor(45.8)); //45
+console.log(Math.ceil(45.1)); //46
+console.log(Math.random() * 10);
